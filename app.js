706d9103207d27912a0164ccb60fa8ccd0f7c49c
Rama <ramiroquesadaperez@gmail.com>
2023-03-15 21:42:19 -0300

    Proceso de consumir API cambiado a Async/Await y agregadas imagenes de carga y error

--- app.js
+++ app.js
@@ -6,65 +6,58 @@ const searchGif = document.getElementById("searchGif");
 
 let inputVar = "";
 
-const apiSearch = "https://api.giphy.com/v1/gifs/translate?api_key=zAaYz6YcvTTEM12q18K7DQD81i2O71zZ&s="
+const apiSearch =
+  "https://api.giphy.com/v1/gifs/translate?api_key=zAaYz6YcvTTEM12q18K7DQD81i2O71zZ&s=";
 
 let imgActualLink = "";
 
 let nuevaImgLink = "";
 
-
-
-searchGif.addEventListener("keydown", function (e){
-	if(e.key == "Enter"){
-		e.preventDefault();
-		inputVar = searchGif.value;
-		changeGif(inputVar)
-		searchGif.value = "";
-	}
+searchGif.addEventListener("keydown", function (e) {
+  if (e.key == "Enter") {
+    e.preventDefault();
+    inputVar = searchGif.value;
+    changeGif(inputVar);
+    searchGif.value = "";
+  }
 });
 
+async function changeGif(opt) {
+  img.src = "./loading.gif";
 
+  try {
+    const response = await fetch(apiSearch + opt, { mode: "cors" });
+    const searchData = await response.json();
 
-changeGif = (opt) => {
-  fetch(
-    apiSearch + opt,
-    { mode: "cors" }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
+    let searchingDiv = document.getElementById("searchingDiv");
+    searchingDiv.innerHTML = "";
 
-			let searchingDiv = document.getElementById("searchingDiv");
-			searchingDiv.innerHTML = "";
-			
-			if (opt != "welcome"){
-				let searchingText = document.createElement("h3");
-				searchingText.innerText = `Buscando: ${opt}`;
-				searchingDiv.append(searchingText);
-			}
-			
+    if (opt != "bienvenidos") {
+      let searchingText = document.createElement("h3");
+      searchingText.innerText = `Buscando: ${opt}`;
+      searchingDiv.append(searchingText);
+    }
 
-			img.src = response.data.images.original.url;
+    img.src = searchData.data.images.original.url;
+  } catch (error) {
+    img.src = "./error.gif";
+    let searchingText = document.createElement("h3");
+    searchingDiv.innerHTML = ``;
+    if (opt == "") {
+      searchingText.innerText = `Error: Ingresa un texto`;
+      searchingDiv.append(searchingText);
+    } else {
+      searchingText.innerText = `No se encontraron resultados para: ${opt}`;
+      searchingDiv.append(searchingText);
+    }
+  }
+}
 
-    })
-		.catch(function(error){
-			if( opt == ""){
-				let searchingText = document.createElement("h3");
-				searchingText.innerText = `Ingresa un texto`;
-				searchingDiv.append(searchingText);
-			}else{
-			alert(`No se encontraron resultados para: ${opt}`)}
-			}
-		);
-			
-};
+changeGif("bienvenidos");
 
-changeGif("welcome");
-
-btnRefresh.addEventListener("click", function(){
-	if(inputVar === ""){
-		inputVar = "welcome"
-	}
-	changeGif(inputVar)
+btnRefresh.addEventListener("click", function () {
+  if (inputVar === "") {
+    inputVar = "bienvenidos";
+  }
+  changeGif(inputVar);
 });
